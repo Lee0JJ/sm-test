c5560d10eb6ba70a4eb4974d0115cea33820ad18
leejj <leejj@sunway.com.my>
2025-01-23 23:53:47 +0800

    Continue with Tour

--- app.js
+++ app.js
@@ -26,13 +26,13 @@ app.get("/home", (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'))
 });
 
-app.get("/destination", (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/destination.html'))
-  });
+app.get("/destination/:country", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/destination.html'))
+});
 
-  app.get("/destination", (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/destination.html'))
-  });
+app.get("/tour/:id", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/tour.html'))
+});
 
 app.listen(5000, () => {
   console.log('Listening on port ' + 5000);
